c93345b12018bea2513f3a8f8094d3f7ace4b92e
dodamce <3030949391@qq.com>
2024-03-24 21:37:29 +0800

    重新放入命名空间

--- util/json.hpp
+++ util/json.hpp
@@ -4,29 +4,32 @@
 #include <memory>
 #include <jsoncpp/json/json.h>
 // 封装json API
-class JsonUtil
+namespace CloudBackups
 {
-public:
-    // 序列化
-    static bool serialize(const Json::Value &root, std::string &str)
+    class JsonUtil
     {
-        Json::StreamWriterBuilder builder;
-        std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
-        std::stringstream ss;
-        writer->write(root, &ss);
-        str = ss.str();
-        return true;
-    }
-    static bool unserialize(std::string &str, Json::Value &write)
-    {
-        Json::CharReaderBuilder builder;
-        std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
-        std::string errs;
-        if (!reader->parse(str.c_str(), str.c_str() + str.size(), &write, &errs))
+    public:
+        // 序列化
+        static bool serialize(const Json::Value &root, std::string &str)
         {
-            std::cerr << "parse json error: " << errs << std::endl;
-            return false;
+            Json::StreamWriterBuilder builder;
+            std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+            std::stringstream ss;
+            writer->write(root, &ss);
+            str = ss.str();
+            return true;
         }
-        return true;
-    }
-};
+        static bool unserialize(std::string &str, Json::Value &write)
+        {
+            Json::CharReaderBuilder builder;
+            std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+            std::string errs;
+            if (!reader->parse(str.c_str(), str.c_str() + str.size(), &write, &errs))
+            {
+                std::cerr << "parse json error: " << errs << std::endl;
+                return false;
+            }
+            return true;
+        }
+    };
+}
